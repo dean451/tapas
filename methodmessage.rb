5ed8bfe1b90da3d430d7a395b3766cfa1fb87f19
Dean Palm <dean451@gmail.com>
2017-10-27 12:45:24 -0500

    method message with a tea clock

---
+++ methodmessage.rb
@@ -0,0 +1,18 @@
+# ruby methods are not first class objects 
+
+# mymethod = def hello
+#              puts "hello, world"
+#            end
+# mymethod # => nil
+
+# class Greeter
+#   def hello
+#     puts "hello, world"
+#   end
+# end
+#
+# greeter = Greeter.new
+# m = greeter.method(:hello)
+# p greeter.hello
+# p m
+# m.call
