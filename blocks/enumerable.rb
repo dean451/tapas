43f21dfab8405c9ee078fd8d37caa7cfe97ee0ea
Dean Palm <dean451@gmail.com>
2017-11-16 14:51:18 -0600

    blocks

--- blocks/enumerable.rb
+++ blocks/enumerable.rb
@@ -24,13 +24,29 @@ orders << Order.new("customer2@example.com", "AZ", 400, :completed)
 orders << Order.new("customer3@example.com", "CO", 200)
 orders << Order.new("customer4@example.com", "CO", 100, :completed)
 
-puts orders
-
 puts "Big orders:"
 puts orders.select { |o| o.total >= 300 }
 puts "Small orders:"
 puts orders.reject { |o| o.total >= 300 }
-puts orders.any? {|o| o.status == :pending}
+
+# puts orders.any? {|o| o.status == :pending} => true
+# puts orders.any? {|o| o.status == :completed} => true
+
 order = orders.detect { |o| o.status == :pending}
 puts order
-puts orders.any? {|o| o.status == :completed}
+
+# partition assigns items in array to first variable for when true is returned
+
+pending_orders, completed_orders = orders.partition { |o| o.status == :pending  }
+puts "Pending:"
+puts pending_orders
+puts "Completed:"
+puts completed_orders
+
+big_orders, small_orders = orders.partition { |o| o.total >= 300  }
+puts "Big:"
+puts big_orders
+puts "Small:"
+puts small_orders
+
+puts "newsletters"
